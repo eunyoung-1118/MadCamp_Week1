338376cdc067ff126aa8de0ec6008ae870ed7a12
eun-young <eunyoung12@kaist.ac.kr>
2024-07-01 20:29:54 +0900

    <feat> db를 사용하여 전화번호부 구현 db를 사용하여 전화번호부 구현

---
+++ app/src/main/java/com/example/madcamp_week1/data/repository/PhoneBookRepository.kt
@@ -0,0 +1,25 @@
+package com.example.madcamp_week1.data.repository
+
+import android.util.Log
+import com.example.madcamp_week1.data.db.PhoneBook
+import com.example.madcamp_week1.data.db.PhoneBookDao
+
+class PhoneBookRepository(private val phoneBookDao: PhoneBookDao) {
+
+    suspend fun insert(phoneBook: PhoneBook) {
+        phoneBookDao.insert(phoneBook)
+    }
+
+    suspend fun update(phoneBook: PhoneBook) {
+        phoneBookDao.update(phoneBook)
+    }
+
+    suspend fun getAllContact(): List<PhoneBook> {
+        return phoneBookDao.getAllContact()
+    }
+
+    suspend fun getContact(id: Long): PhoneBook {
+        return phoneBookDao.getContact(id)
+    }
+
+}
---
+++ app/src/main/java/com/example/madcamp_week1/phoneBookAdapter.kt
@@ -0,0 +1,39 @@
+package com.example.madcamp_week1
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.ImageView
+import android.widget.TextView
+import com.example.madcamp_week1.data.db.PhoneBook
+
+class phoneBookAdapter(private var contacts: List<PhoneBook>) : BaseAdapter() {
+
+    override fun getCount(): Int = contacts.size
+
+    override fun getItem(position: Int): PhoneBook = contacts[position]
+
+    override fun getItemId(position: Int): Long = position.toLong()
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        val view = convertView ?: LayoutInflater.from(parent?.context).inflate(R.layout.phone_item, parent, false)
+        val contact = contacts[position]
+
+        val nameTextView = view.findViewById<TextView>(R.id.nameItem)
+        val numTextView = view.findViewById<TextView>(R.id.numItem)
+        val contactImageView = view.findViewById<ImageView>(R.id.contact_image)
+
+        nameTextView.text = contact.name
+        numTextView.text = contact.num
+        // Set photoUri if available
+
+        return view
+    }
+
+    // 데이터 업데이트 메서드
+    fun updateData(newContacts: List<PhoneBook>) {
+        contacts = newContacts
+        notifyDataSetChanged() // 데이터가 변경되었음을 어댑터에 알림
+    }
+}
